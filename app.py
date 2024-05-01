19ef974f64df2d5acc97bce28a9b950961182a4b
Isaias Santos <isaiassousa032@gmail.com>
2024-05-01 18:14:48 -0300

    cria o método para adicionar novas missoes

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask 
+from asyncio import tasks
+from flask import Flask, jsonify, request 
 from models.missao import Missao
 
 # Cria uma nova instância da classe Flask, atribuindo-a à variável app
 # o __name__ tem valor __main__
 app = Flask(__name__) 
 
-missoes = []
-
-
+missions = [] #lista vazia que armazenará as missões criadas
+mission_id_control = 1 #Uma variável global que controla o ID da próxima missão a ser criada, iniciando em 1.
+@app.route('/missions', methods=['POST'])
+def create_mission():
+    global mission_id_control
+    data = request.get_json() #recupera os dados JSON enviados no corpo da requisição POST.
+    new_mission = Missao(id=mission_id_control, nome_da_missao=data.get('nome_da_missao'), data_de_lancamento=data.get('data_de_lancamento'), destino=data.get('destino'), estado=data.get('estado'), tripulacao=data.get('tripulacao'), duracao=data.get('duracao'), status=data.get('status'), carga_util=data.get('carga_util'))
+    mission_id_control += 1
+    missions.append(new_mission)
+    print(missions)
+    return jsonify({"message": "Nova missão criada com sucesso."})
 if __name__ == "__main__":
-    app.run(host='localhost', debug=True, port=2961) 
+    app.run(host='127.0.0.1', debug=True, port=2961) 
--- models/missao.py
+++ models/missao.py
@@ -1,22 +1,24 @@
 class Missao:
-    def __init__(self,id, nome, data_de_lancamento, destino, estado, tripulacao, duracao, status ) -> None:
+    def __init__(self, id, nome_da_missao, data_de_lancamento, destino, estado, tripulacao, duracao, status, carga_util ) -> None:
         self.id = id
-        self.nome = nome
+        self.nome_da_missao = nome_da_missao
         self.data_de_lancamento = data_de_lancamento
         self.destino = destino
         self.estado = estado
         self.tripulacao = tripulacao
         self.duracao = duracao
         self.status = status
+        self.carga_util = carga_util
 
     def para_dicionario(self):
         return {
             "id": self.id,
-            "nome": self.nome,
+            "nome_da_missao": self.nome_da_missao,
             "data da lançamento": self.data_de_lancamento,
             "destino": self.destino,
             "estado": self.estado,
             "tripulação": self.tripulacao,
             "duração": self.duracao,
-            "status": self.status
+            "status": self.status,
+            "carga util": self.carga_util
         }
