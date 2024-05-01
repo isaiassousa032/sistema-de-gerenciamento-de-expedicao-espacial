23631caae667c86a5fe626d90cad9f11705ca487
Isaias Santos <isaiassousa032@gmail.com>
2024-05-01 19:07:56 -0300

    refactor: passado para pt-br

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ from models.missao import Missao
 # o __name__ tem valor __main__
 app = Flask(__name__) 
 
-missions = [] #lista vazia que armazenará as missões criadas
-mission_id_control = 1 #Uma variável global que controla o ID da próxima missão a ser criada, iniciando em 1.
-@app.route('/missions', methods=['POST'])
+missoes = [] #lista vazia que armazenará as missões criadas
+controle_de_missao_pelo_id = 1 #Uma variável global que controla o ID da próxima missão a ser criada, iniciando em 1.
+@app.route('/missoes', methods=['POST'])
 def create_mission():
-    global mission_id_control
+    global controle_de_missao_pelo_id
     data = request.get_json() #recupera os dados JSON enviados no corpo da requisição POST.
-    new_mission = Missao(id=mission_id_control, nome_da_missao=data.get('nome_da_missao'), data_de_lancamento=data.get('data_de_lancamento'), destino=data.get('destino'), estado=data.get('estado'), tripulacao=data.get('tripulacao'), duracao=data.get('duracao'), status=data.get('status'), carga_util=data.get('carga_util'))
-    mission_id_control += 1
-    missions.append(new_mission)
-    print(missions)
+    nova_missao = Missao(id=controle_de_missao_pelo_id, nome_da_missao=data.get('nome_da_missao'), data_de_lancamento=data.get('data_de_lancamento'), destino=data.get('destino'), tripulacao=data.get('tripulacao'), duracao=data.get('duracao'), status_da_missao=data.get('status_da_missao'), carga_util=data.get('carga_util'))
+    controle_de_missao_pelo_id += 1
+    missoes.append(nova_missao)
+    print(missoes)
     return jsonify({"message": "Nova missão criada com sucesso."})
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True, port=2961) 
--- models/missao.py
+++ models/missao.py
@@ -1,24 +1,24 @@
+from datetime import date
+
 class Missao:
-    def __init__(self, id, nome_da_missao, data_de_lancamento, destino, estado, tripulacao, duracao, status, carga_util ) -> None:
+    def __init__(self, id: int, nome_da_missao: str, data_de_lancamento: date, destino: str, tripulacao: str, duracao, status_da_missao: str, carga_util: str ) -> None:
         self.id = id
         self.nome_da_missao = nome_da_missao
         self.data_de_lancamento = data_de_lancamento
         self.destino = destino
-        self.estado = estado
         self.tripulacao = tripulacao
         self.duracao = duracao
-        self.status = status
+        self.status = status_da_missao
         self.carga_util = carga_util
 
     def para_dicionario(self):
         return {
             "id": self.id,
             "nome_da_missao": self.nome_da_missao,
-            "data da lançamento": self.data_de_lancamento,
+            "data da lancamento": self.data_de_lancamento,
             "destino": self.destino,
-            "estado": self.estado,
-            "tripulação": self.tripulacao,
-            "duração": self.duracao,
-            "status": self.status,
+            "tripulacao": self.tripulacao,
+            "duracao": self.duracao,
+            "status_da_missao": self.status,
             "carga util": self.carga_util
         }
