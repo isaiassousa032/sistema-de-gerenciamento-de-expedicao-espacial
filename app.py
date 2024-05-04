3501a26574223c7ae3ecd84e98d646dfc805acdd
Isaias Santos <isaiassousa032@gmail.com>
2024-05-04 18:34:17 -0300

    criação do update e do delete

--- app.py
+++ app.py
@@ -34,5 +34,41 @@ def recupera_missao_por_id(id):
             return jsonify(m.para_dicionario())
     return jsonify({"message": "Missão não encontrada"}), 404
 
+@app.route('/missoes/<int:id>', methods=['PUT'])
+def atualiza_missao(id):
+    missao = None #é adicionado porque pode ou não, ter uma tarefa e se nao tiver retorna None
+    for m in missoes:
+        if m.id == id:
+            missao = m
+            break
+    
+    if missao == None:
+        return jsonify({"message": "Missão não encontrada"}), 404 #404 é uma mensagem do servidor, avisando que o recurso/página não foi encontrado.
+    
+    data = request.get_json()
+    missao.nome_da_missao = data['nome_da_missao']
+    missao.data_de_lancamento = data['data_de_lancamento']
+    missao.destino = data['destino']
+    missao.tripulacao = data['tripulacao']
+    missao.duracao = data['duracao']
+    missao.status_da_missao = data['status_da_missao']
+    missao.carga_util = data['carga_util']
+    
+    return jsonify({"message":"Missão atualizada com sucesso."}), 200
+           
+@app.route('/missoes/<int:id>', methods=['DELETE'])
+def deleta_missao(id):
+    missao = None
+    for m in missoes:
+        if m.id == id:
+            missao = m
+            break #é usado para não precisar percorrer todo o array mesmo tendo achado o elemento procurado, assim economizando processamento
+
+    if not missao:
+        return jsonify({"message": "Missão não encontrada"}), 404
+    
+    missoes.remove(missao)
+    return jsonify({"message": "Missão deletada com sucesso"}), 200
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True, port=2961) 
